aa17f4915fd6f20caaa6bb7e1a3fbbfa173fd5d0
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-05 22:47:06 -0400

    installed chalk library

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk')
 const getNotes = require('./notes')
 
 const msg = getNotes()
@@ -6,7 +7,17 @@ console.log(msg)
 
 
 
+// CHALLENGE USE THE CHALK LIBRARY IN YOUR PROJECT
+// 1. INSTALL VERSION 2.4.2 OF CHALK done
 
-// const guy = require('./utils')
-// const sum = guy(5, 10)
-// console.log(sum)
+// 2. LOAD CHALK INTO APP.JS done
+
+// 3. USE IT TO PRINT THE STRING "SUCCESS!" TO THE CONSOLE IN GREEN done
+console.log(chalk.green('success!'))
+console.log(chalk.green.bold('success!'))
+console.log(chalk.green.bold.inverse('success!'))
+// 4. TEST YOUT WORK done
+
+// BONUS: USE DOCS TO MESS AROUND WITH OTHER STYLES. MAKE TEXT BOLD AND INVERSES
+
+
