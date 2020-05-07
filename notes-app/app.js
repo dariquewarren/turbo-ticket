18e69bcb307ca6f0e3ed5e2b693ae40c32c5ec7d
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-07 18:47:49 -0400

    adjusting files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
-
+const notes = require('./notes.js')
+const command = process.argv
 // Customize yargs version
 yargs.version('1.1.0')
 
@@ -22,8 +22,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -54,4 +53,3 @@ yargs.command({
     }
 })
 
-yargs.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,28 @@
+const fs = require('fs')
+
 const getNotes = function () {
     return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+const notes = loadNotes()
+console.log(notes)
+}
+
+const loadNotes = function(){
+   try {
+const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+   }catch(e){
+    return []
+   }
+   
+   
+    
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
