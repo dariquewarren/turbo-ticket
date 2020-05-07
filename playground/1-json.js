bc2b8a86f88b1fd18b1282d42aa5ccac4aed8ced
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-06 20:19:07 -0400

    messing around in playground

---
+++ playground/1-json.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+
+
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+
+//
+// CHALLENGE: WORK WITH JSON AND THE FILE SYSTEM
+
+//1.    LOAD AND PARSE THE JSON DATA
+//2.    CHANGE THE NAME AND AGE PROPERTY USING YOUR INFO
+//3. STRINGIFY THE CHANGED OBJECT AND OVERWRITE THE ORIGINAL DATA
+//4. TEST YOUR WORK BY VIEWING DATA IN THE JSON FILE
