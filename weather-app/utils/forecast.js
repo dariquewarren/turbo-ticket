7f95bed02c35fc15cb2f9a413479c4451380f4a5
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-17 19:33:39 -0400

    made a note in forecast to clean the code up before a pr submission

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,6 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
+    // use weatherStack api instead of darksky
     const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
