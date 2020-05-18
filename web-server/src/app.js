c2b00846e70207766b33af19427b300266d496d7
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-18 13:40:14 -0400

    added some comments, used get method from express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,31 +1,47 @@
 const express = require('express')
 
 const app = express()
-
+// uses express' get method to send a response to the page specified by quotes.
+// empty quotes seems to mean root folder of 
+// res.send sends whatever it's called with to the page specified by the function
 app.get('', (req, res)=> {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res)=>{
-    res.send('Help page')
+    res.send([{
+        name: 'darique',
+        age: 31
+    }] )
 })
 
+
+/* 
+
+GOAL: UPDATE ROUTES
+
+1. SETUP ABOUT ROUTE TO RENDER A TITLE WITH HTML
+2. SETUP A WEATHER ROUTE TO SEND BACK json
+    - OBJECT WITH FORECAST AND LOCATION STRINGS
+3. TEST YOUR WORK BY VISITING BOTH IN THE BROWSER
+
+*/
 app.get('/about', (req, res)=>{
-    res.send('About our team')
+    res.send('<h3>ABOUT OUR TEAM<h3>')
 })
- app.get('/weather', (req, res)=>{
-    res.send('Current Weather')
+
+
+
+
+app.get('/weather', (req, res)=>{
+    res.send( {
+        forecast: 'It is weather',
+        location: 'It is here'
+    })
  })
 
 
-// goal: setup two new routes
-// 1. setup an about route and render a page title
-// 2. setup a weather route and render a page title
-// 3. test your work by visiting both in the browser
 
-// app.com
-// app.com/help
-// app.com/about
 
 app.listen(3000, ()=>{
     console.log('server is up on port 3000')
