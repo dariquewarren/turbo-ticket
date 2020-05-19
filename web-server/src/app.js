8f54c296d6d36717735ebb7fb1c28aeec7096e51
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-19 19:08:21 -0400

    created handlebars files for help, index and about. used express to load those pages via render method on response parameter within app.get() in app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -14,6 +14,36 @@ app.use(express.static(publicDirectoryPath))
 
 app.set('view engine','hbs')
 
+app.get('', (req, res)=>{
+    res.render('index', {
+        title: 'Weather',
+        name: 'Darique Warren'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about', {
+        title: 'About Me',
+        name: 'Darique Warren'
+    })
+})
+
+/* 
+GOAL: CREATE A TEMPLATE FOR HELP PAGE
+
+1. SETUP A HELP TEMPLATE TO RENDER A HELP MESSAGE TO THE SCREEN 
+2. SETUP THE HELP ROUTE AND RENDER THE TEMPLATE WITH AN EXAMPLE MESSAGE
+3. VISIT THE ROUTE IN THE BROWSER AND SEE YOUR HELP MESSAGE PRINT
+
+*/
+
+app.get('/help', (req, res)=>{
+res.render('help', {
+    title:'HEEEEEEELLLLLLPPPP IS BELOW',
+    helpMessage: 'These are not the droids you are looking for'
+})
+})
+
 
 app.get('/weather', (req, res)=>{
     res.send( {
