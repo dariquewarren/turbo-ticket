1f33ec419bd2dd2a2117c9dc18366fd1b6124c93
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-19 14:55:50 -0400

    added images, css and a separate js file

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,10 +3,15 @@ const express = require('express')
 
 // turns expressfunction into a usable variable
 const app = express()
+
+
 // creates a variable that represents file path FOR OUBLIC DIRECTORY
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+
 // loads static page via the exoress method 'get' and exoress method 'static'
 app.use(express.static(publicDirectoryPath))
+
 
 
 
