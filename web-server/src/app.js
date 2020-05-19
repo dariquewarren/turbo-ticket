53129859be4896923e1a45136635a51d21e66894
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-19 18:31:12 -0400

    PREPPED AND INSTALLED HANDLEBARS

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -12,38 +12,7 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 // loads static page via the exoress method 'get' and exoress method 'static'
 app.use(express.static(publicDirectoryPath))
 
-
-
-
-
-
-
-/* 
-
-GOAL: CREATE TWO MORE HTML FILES
-
-1. CREATE A HTML PAGE FOR ABOUT WITH "ABOUT" TITLE
-2. CREATE A HTML PAGE FOR HELO WITH "HELP" TITLE
-3. REMOVE THE OLD ROUTE HANDLERS FOR BOTH
-4. VISIT BOTH IN THE BROWSER TO TEST YOUR WORK
-
-
-
-// creates a variable that represents file path FOR OUBLIC DIRECTORY
-const helpPagePath = path.join(__dirname, '../public')
-// loads static page via the express method 'get' and exoress method 'static'
-app.use(express.static(helpPagePath))
-
-
-
-
-// creates a variable that represents file path FOR OUBLIC DIRECTORY
-const aboutPagePath = path.join(__dirname, '../public')
-// loads static page via the express method 'get' and express method 'static'
-app.use(express.static(helpPagePath))
-
-
-*/
+app.set('view engine','hbs')
 
 
 app.get('/weather', (req, res)=>{
