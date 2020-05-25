b3ccf41efbd5511fb350a53a1788865defb5cfda
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-25 13:06:05 -0400

    added forecast and geocode files from another directory to be used later

---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    // use weatherStack api to take a longtitude and latitude and respond with weather information for that area
+const url =  `http://api.weatherstack.com/current?access_key=9fdf17fc0144d6830d27103e37c7b012&query=${latitude},${longitude}&units=f` 
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback( body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.', undefined)
+        }
+    })
+}
+
+module.exports = forecast
