2bc5a5655c9ff73a04693ebacf29982bc9385327
DamienFBlack <60529780+DamienFBlack@users.noreply.github.com>
2020-05-25 19:38:53 -0400

    made a fetch cal for mapbox and tested it

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -5,10 +5,23 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
         console.log(data)
     })
 })
-
-
+const locationTest = 'Harlem'
+fetch(`http://api.mapbox.com/geocoding/v5/mapbox.places/${locationTest}.json?access_token=pk.eyJ1IjoiZGFyaXF1ZXciLCJhIjoiY2thYnp4YWdyMDdodDMxbXcwOGsxcnA2ciJ9.XEueTGOscJKBKl1IZZiOiQ&limit=1`).then((response)=> {
+    response.json().then((data)=>{
+        
+                 
+        console.log(data.features[0].center[1])
+         console.log(data.features[0].center[0])
+    })
+})
 /*
 
 GOAL: FETCH WEATHER!
+
+1. SETUP A CALL TO FETCH WEATHER FOR BOSTON
+2.GET THE PARSED json RESPONSE
+        -IF ERROR PROPERTY, PRINT ERROR
+        -IF NO ERROR PROPERTY, PRINT LOCATION AND FORECAST
+3. REFRESH THE BROWSER AND TEST YOUR WORK
 
 */
