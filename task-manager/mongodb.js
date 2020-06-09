0de8d420187e8ab1dc1a4aa9a55fe931a97894f2
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-06-09 16:12:20 -0400

    successfully used find with boolean param

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -46,4 +46,15 @@ if (error){
 }
     console.log(task)
 })
+
+db.collection('tasks').find({completed: false}, (error, task)=>{
+    if(error) {
+      console.log('unable to fetch')  
+    }
+
+console.log(task)
+
+})
+
+
 })
