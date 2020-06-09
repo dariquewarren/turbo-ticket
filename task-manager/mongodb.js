76fe81fab098c1376fa40bbfd4c1c58ced6f1867
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-06-09 16:08:51 -0400

    succesfully used findone with id param

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -19,12 +19,31 @@ if (error){
 }
 
   const db = client.db(databaseName)
-db.collection('users').findOne({_id: new ObjectID('5edfa5bffd3b2918e087b265')}, (error, user)=>{
-if(error) {
-    console.log('user error')
+// db.collection('users').findOne({_id: new ObjectID('5edfa5bffd3b2918e087b265')}, (error, user)=>{
+// if(error) {
+//     console.log('user error')
+// }
+
+// console.log(user)
+// })
+
+// db.collection('users').find({age: 31}).count((error, count)=>{
+//     console.log(count)
+// })
+
+/*
+GOAL 
+USE FIND AND FIND ONE WITH TASKS
+1. USE FIND ONE TO FETCH THE LAST TASK BY ID. PRINT TO CONSOLE
+2. USE FIND TO FETCH ALL TASLS THAT ARE NOT COMPLETED
+3. TEST YOUR WORK!
+
+*/
+
+db.collection('tasks').findOne({_id: new ObjectID("5edfa9e104c6c335b46acfc3")}, (error, task) =>{
+if (error){
+    console.log('unable to fetch task')
 }
-
-console.log(user)
+    console.log(task)
 })
-
 })
